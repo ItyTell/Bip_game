5a23d3e9de59f3710745cd110cb5937566dfcc45
ItyTell <nickkolomietsdooor@gmail.com>
2023-04-30 21:20:44 +0300

    adding game class

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame 
+import moderngl
+
+pygame.init()
+
+
+class Game:
+
+    def __init__(self) -> None:
+        self.run = True
+        pass
+
+    def ran(self):
+        while self.run:
+            pass
