b7b5aee5ba3531f94e80a8067d8ff37737afd9f6
ItyTell <nickkolomietsdooor@gmail.com>
2023-05-17 09:52:12 +0300

    adding settings

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame 
 import moderngl
+import json 
+
 
 pygame.init()
 
@@ -8,8 +10,27 @@ class Game:
 
     def __init__(self) -> None:
         self.run = True
-        pass
+        self.load_settings()
+        self.screen = pygame.display.set_mode(self.screen_params)
+        self.clock = pygame.time.Clock()    
+        self.ran()
+    
+    def load_settings(self) -> None:
+        with open('settings.json') as file:
+            settings = json.loads(file.read())
+        self.screen_params = (settings["screen"]["width"], settings["screen"]["height"])
+        file.close()
 
     def ran(self):
         while self.run:
-            pass
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.run = False
+            self.screen.fill("purple")
+            pygame.display.flip()
+            self.clock.tick(60)
+        pygame.quit()
+
+
+
+Game()
