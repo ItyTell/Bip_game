7d5f96b000aa293e30bbd44aeb0566e590065ec4
ItyTell <nickkolomietsdooor@gmail.com>
2023-06-10 18:21:23 +0300

    diagram + making sructure

--- entity.py
+++ entity.py
@@ -2,13 +2,14 @@ import pygame
 import os
 from math import floor
 
-class Entity:
+class Entity(pygame.sprite.Sprite):
     entitys = []
 
     def __init__(self, name, cords) -> None:
+        super.__init__(self)
         self.name = name
         self.load_imgs()
-        self.cords = cords 
+        self.rect = cords 
         self.anim_index = 0
         self.anim_speed = 0.25
         Entity.entitys.append(self)
@@ -17,7 +18,7 @@ class Entity:
         self.idl = []
         path = "imgs"
         for i in range(10):
-            img = pygame.image.load(path + "\\" + self.name + "_idl" + str(i + 1) + ".bmp").convert_alpha()
+            img = pygame.image.load(path + "\\" + self.name + "\\" + "idl" + "\\" + str(i + 1) + ".bmp").convert_alpha()
             img = pygame.transform.scale(img, (img.get_width()*3, img.get_height()*3))
             img.set_colorkey((0, 0, 0))
             self.idl.append(img)
@@ -26,3 +27,9 @@ class Entity:
         screen.blit(self.idl[floor(self.anim_index)], self.cords)
         self.anim_index += self.anim_speed
         self.anim_index -= len(self.idl) if self.anim_index >= len(self.idl) else 0
+
+
+class Animation():
+
+    def __init__(self) -> None:
+        pass
